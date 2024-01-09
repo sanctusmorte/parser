08fff5d34925bb4639554269992c8a490754f867
root <sanctusmortes@gmail.com>
2024-01-09 17:53:53 +0500

    fix

--- app/Services/Thumbs/ThumbsService.php
+++ app/Services/Thumbs/ThumbsService.php
@@ -225,7 +225,7 @@ class ThumbsService
             $foundPornStarsCount = $this->getPornStarsCountByHrefTitles($needTitles);
         }
 
-        dd($foundTagsCount, $needTitles, $foundPornStarsCount, $links);
+        dd($foundTagsCount, $needTitles, $foundPornStarsCount, $links, $dataTitles);
 
         if ($foundTagsCount/count($needTitles) >= 0.3 or $foundPornStarsCount/count($needTitles) >= 0.3) {
             return true;
